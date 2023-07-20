72498d603667c730b4885fbe091b78082a940c4d
Igor S. Morozov <igor@morozov.is>
2023-07-20 17:11:37 +0300

    Fix style

--- lua/git_browse.lua
+++ lua/git_browse.lua
@@ -44,10 +44,10 @@ M.browse = function()
 end
 
 M.browse_line = function()
-  data = module.browse_line(M.config.git_branch_command, M.config.git_remote_get_url_command, M.config.git_repo_root_command)
+  data =
+    module.browse_line(M.config.git_branch_command, M.config.git_remote_get_url_command, M.config.git_repo_root_command)
   return os.execute(data.cmd .. " " .. data.url)
 end
-
 
 M.blame = function()
   data = module.blame(M.config.git_branch_command, M.config.git_remote_get_url_command, M.config.git_repo_root_command)
@@ -55,7 +55,8 @@ M.blame = function()
 end
 
 M.blame_line = function()
-  data = module.blame_line(M.config.git_branch_command, M.config.git_remote_get_url_command, M.config.git_repo_root_command)
+  data =
+    module.blame_line(M.config.git_branch_command, M.config.git_remote_get_url_command, M.config.git_repo_root_command)
   return os.execute(data.cmd .. " " .. data.url)
 end
 
